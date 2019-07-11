ba53bd0b70969321ec8c2236c7e8de9eca1cf4e7
Jake Shams <jshams62@gmail.com>
2019-07-10 19:39:30 -0700

    split lines method

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,4 +1,3 @@
-# !Python
 from linkedlist import LinkedList
 
 class Vertex(object):
@@ -9,16 +8,42 @@ class Vertex(object):
     def points_to(self, pointer):
         self.pointers.append(pointer)
 
-class Graph(object):
+class ADTGraph(object):
     def __init__(self, FILE_PATH = ''):
+        self.FILE_PATH = FILE_PATH
+        self.vertices = [] # this is a list of verticies
+        self.nodes = [] # nodes are all the verticies' datas
+
+    def read_from_g(self, FILE_PATH):
         pass
+
+    def split_line(self, line):
+        v1 = ''
+        v2 = ''
+        first = True
+        for char in line:
+            if char == ',':
+                first = False
+            elif char != '(' and char != ')':
+                if first:
+                    v1 += char
+                else:
+                    v2 += char
+        if v1.isdigit():
+            v1 = int(v1)
+        if v2.isdigit():
+            v2 = int(v2)
+        return v1, v2
 
     def add_vertex(self, vert):
         '''adds an instance of Vertex to the graph.'''
+        self.vertices.append(Vertex(vert))
+
+    def add_edge(self, fromVert, toVert):
+        '''Adds a new, directed edge to the graph that connects two vertices.'''
         pass
-    def add_edge(self, fromVert, toVert, weight = None):
-        '''If weight is None: Adds a new, directed edge to the graph that connects two vertices.
-        Else: Adds a new, weighted, directed edge to the graph that connects two vertices.'''
+    def add_edge(self, fromVert, toVert, weight):
+        '''Adds a new, weighted, directed edge to the graph that connects two vertices.'''
         pass
     def get_vertex(self, vertKey):
         '''finds the vertex in the graph named vertKey.'''
@@ -29,3 +54,13 @@ class Graph(object):
     def get_neighbors(self, x): 
         '''lists all vertices y such that there is an edge from the vertex x to the vertex y.'''
         pass
+
+
+    
+
+
+def test_ADTGraph():
+    print(ADTGraph().split_line('(12,353)'))
+
+if __name__ == '__main__':
+    test_ADTGraph()
