533cdf6e3c813c697c0c43abd0ba383991967ebd
Jake Shams <jshams62@gmail.com>
2019-07-12 16:48:05 -0700

    implemented methods

--- graphs/graph.py
+++ graphs/graph.py
@@ -12,9 +12,38 @@ class ADTGraph(object):
     def __init__(self, FILE_PATH = ''):
         self.FILE_PATH = FILE_PATH
         self.vertices = [] # this is a list of verticies
-        self.nodes = [] # nodes are all the verticies' datas
+    
+    def __iter__(self):
+        for vertex in self.vertices:
+            yield vertex
 
-    def read_from_g(self, FILE_PATH):
+    def add_vertex(self, vert):
+        '''adds an instance of Vertex to the graph.'''
+        self.vertices.append(Vertex(vert))
+
+    def add_edge(self, fromVert, toVert, weight = None):
+        '''Adds a new, weighted, directed edge to the graph that connects two vertices.'''
+        fromVert.points_to(toVert)
+
+    def get_vertex(self, vertKey):
+        '''finds the vertex in the graph named vertKey.'''
+        for vertex in self.vertices:
+            if vertex.data == vertKey:
+                return vertex
+
+    def get_vertices(self): 
+        '''returns the list of all vertices in the graph.'''
+        return [vertex.data for vertex in self]
+
+    def get_neighbors(self, x): 
+        '''lists all vertices y such that there is an edge from the vertex x to the vertex y.'''
+        return [pointer for pointer in x.pointers]
+
+class FileReader(object):
+    def __init__(self, PATH):
+        self.PATH = PATH
+    
+    def read_from_g(self):
         pass
 
     def split_line(self, line):
@@ -34,30 +63,6 @@ class ADTGraph(object):
         if v2.isdigit():
             v2 = int(v2)
         return v1, v2
-
-    def add_vertex(self, vert):
-        '''adds an instance of Vertex to the graph.'''
-        self.vertices.append(Vertex(vert))
-
-    def add_edge(self, fromVert, toVert):
-        '''Adds a new, directed edge to the graph that connects two vertices.'''
-        pass
-    def add_edge(self, fromVert, toVert, weight):
-        '''Adds a new, weighted, directed edge to the graph that connects two vertices.'''
-        pass
-    def get_vertex(self, vertKey):
-        '''finds the vertex in the graph named vertKey.'''
-        pass
-    def get_vertices(self): 
-        '''returns the list of all vertices in the graph.'''
-        pass
-    def get_neighbors(self, x): 
-        '''lists all vertices y such that there is an edge from the vertex x to the vertex y.'''
-        pass
-
-
-    
-
 
 def test_ADTGraph():
     print(ADTGraph().split_line('(12,353)'))
