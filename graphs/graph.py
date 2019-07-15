b53603e7b9fad1f662d81a8177d6df6eb22c8057
Jake Shams <jshams62@gmail.com>
2019-07-14 22:17:15 -0700

    removed vertex and filereader classes

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,19 +1,6 @@
-from linkedlist import LinkedList
-
-class Vertex(object):
-    def __init__(self, data):
-        self.data = data
-        self.pointers = LinkedList()
-
-    def points_to(self, pointer):
-        self.pointers.append(pointer)
-    
-    def is_pointing_to(self, pointer):
-        return pointer in self.pointers
-
 class ADTGraph(object):
     def __init__(self, FILE_PATH = ''):
-        self.FILE_PATH = FILE_PATH
+        self.f = FileReader(FILE_PATH)
         self.vertices = [] # this is a list of verticies
     
     def __iter__(self):
@@ -24,12 +11,19 @@ class ADTGraph(object):
         '''adds an instance of Vertex to the graph.'''
         self.vertices.append(Vertex(vert))
 
-    def add_edge(self, fromVert, toVert, weight = None):
+    def add_edge(self, from_vert, to_vert, weight = 1):
         '''Adds a new, weighted, directed edge to the graph that connects two vertices.'''
-        fromVert.points_to(toVert)
+        from_vert_vert = self.get_vertex(from_vert)
+        to_vert_vert = self.get_vertex(to_vert)
+        if from_vert_vert is None:
+            raise KeyError('from_vert key not found in graph')
+        elif to_vert_vert is None:
+            raise KeyError('to_vert key not found in graph')
+        else:
+            from_vert_vert.points_to(to_vert_vert)
 
     def get_vertex(self, vertKey):
-        '''finds the vertex in the graph named vertKey.'''
+        '''finds the vertex in the graph named vertKey. Or returns None if not found'''
         for vertex in self.vertices:
             if vertex.data == vertKey:
                 return vertex
@@ -41,34 +35,6 @@ class ADTGraph(object):
     def get_neighbors(self, x): 
         '''lists all vertices y such that there is an edge from the vertex x to the vertex y.'''
         return [pointer for pointer in x.pointers]
-
-class FileReader(object):
-    def __init__(self, PATH = None):
-        self.PATH = PATH
-    
-    def read_from_g(self):
-        pass
-
-    def split_line(self, line):
-        v1 = ''
-        v2 = ''
-        first = True
-        for char in line:
-            if char == ',':
-                first = False
-            elif char != '(' and char != ')':
-                if first:
-                    v1 += char
-                else:
-                    v2 += char
-        if v1.isdigit():
-            v1 = int(v1)
-        if v2.isdigit():
-            v2 = int(v2)
-        return v1, v2
-
-def test_FileReader():
-    print(FileReader().split_line('(12,353)'))
 
 if __name__ == '__main__':
     test_FileReader()
