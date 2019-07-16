a3219e0fbcd2c514c23ce0930cea41464b251d24
Jake Shams <jshams62@gmail.com>
2019-07-15 23:18:35 -0700

    graphs now have vertex_count and edge_count properties

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,11 +1,14 @@
 from vertex import Vertex
 from file_reader import FileReader
 
+
 class ADTGraph(object):
-    def __init__(self, FILE_PATH = ''):
+    def __init__(self, FILE_PATH=''):
         self.f = FileReader(FILE_PATH)
-        self.vertices = {} # dictionary of verticies
-    
+        self.vertices = {}  # dictionary of verticies
+        self.vertex_count = 0
+        self.edge_count = 0
+
     def __iter__(self):
         '''yields each vertex key'''
         for vertex in self.vertices:
@@ -14,8 +17,9 @@ class ADTGraph(object):
     def add_vertex(self, vert):
         '''adds an instance of Vertex to the graph.'''
         self.vertices[vert] = Vertex(vert)
+        self.vertex_count += 1
 
-    def add_edge(self, from_vert_key, to_vert_key, weight = 1):
+    def add_edge(self, from_vert_key, to_vert_key, weight=1):
         '''Adds a new, weighted, directed edge to the graph that connects two vertices.'''
         from_vert = self.get_vertex(from_vert_key)
         to_vert = self.get_vertex(to_vert_key)
@@ -25,19 +29,21 @@ class ADTGraph(object):
             raise KeyError(f'{to_vert} key not found in graph')
         else:
             from_vert.points_to(to_vert)
+            self.edge_count += 1
 
     def get_vertex(self, vert_key):
         '''finds the vertex in the graph named vertKey. Or returns None if not found'''
         if vert_key in self.vertices:
             return self.vertices[vert_key]
 
-    def get_vertices(self): 
+    def get_vertices(self):
         '''returns the list of all vertices in the graph.'''
         return [vertex for vertex in self]
 
     def get_neighbors(self, x):
         '''lists all vertices y such that there is an edge from the vertex x to the vertex y.'''
         return [pointer for pointer in x.pointers]
+
 
 if __name__ == '__main__':
     pass
