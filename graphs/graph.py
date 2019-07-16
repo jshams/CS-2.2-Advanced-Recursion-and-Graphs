94f2855c94decfc2671aa63e89c2523b54bcd5ce
Jake Shams <jshams62@gmail.com>
2019-07-15 22:53:22 -0700

    vertices stored in a dictionary instead of dynamic array

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,38 +1,41 @@
+from vertex import Vertex
+from file_reader import FileReader
+
 class ADTGraph(object):
     def __init__(self, FILE_PATH = ''):
         self.f = FileReader(FILE_PATH)
-        self.vertices = [] # this is a list of verticies
+        self.vertices = {} # dictionary of verticies
     
     def __iter__(self):
+        '''yields each vertex key'''
         for vertex in self.vertices:
             yield vertex
 
     def add_vertex(self, vert):
         '''adds an instance of Vertex to the graph.'''
-        self.vertices.append(Vertex(vert))
+        self.vertices[vert] = Vertex(vert)
 
-    def add_edge(self, from_vert, to_vert, weight = 1):
+    def add_edge(self, from_vert_key, to_vert_key, weight = 1):
         '''Adds a new, weighted, directed edge to the graph that connects two vertices.'''
-        from_vert_vert = self.get_vertex(from_vert)
-        to_vert_vert = self.get_vertex(to_vert)
-        if from_vert_vert is None:
-            raise KeyError('from_vert key not found in graph')
-        elif to_vert_vert is None:
-            raise KeyError('to_vert key not found in graph')
+        from_vert = self.get_vertex(from_vert_key)
+        to_vert = self.get_vertex(to_vert_key)
+        if from_vert is None:
+            raise KeyError(f'{from_vert} key not found in graph')
+        elif to_vert is None:
+            raise KeyError(f'{to_vert} key not found in graph')
         else:
-            from_vert_vert.points_to(to_vert_vert)
+            from_vert.points_to(to_vert)
 
-    def get_vertex(self, vertKey):
+    def get_vertex(self, vert_key):
         '''finds the vertex in the graph named vertKey. Or returns None if not found'''
-        for vertex in self.vertices:
-            if vertex.data == vertKey:
-                return vertex
+        if vert_key in self.vertices:
+            return self.vertices[vert_key]
 
     def get_vertices(self): 
         '''returns the list of all vertices in the graph.'''
-        return [vertex.data for vertex in self]
+        return [vertex for vertex in self]
 
-    def get_neighbors(self, x): 
+    def get_neighbors(self, x):
         '''lists all vertices y such that there is an edge from the vertex x to the vertex y.'''
         return [pointer for pointer in x.pointers]
 
