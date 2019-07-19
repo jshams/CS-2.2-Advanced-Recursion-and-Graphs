ee270cfa35dfd1cbd08775d643e5d5f3e4ca7da4
Jake Shams <jshams62@gmail.com>
2019-07-19 15:10:22 -0700

    made changes after testing

--- graphs/file_reader.py
+++ graphs/file_reader.py
@@ -7,7 +7,7 @@ class FileReader(object):
         verticies = []
         edges = []
         f = open(self.PATH, "r")
-        for i, line in enumerate(f.readlines()):
+        for i, line in enumerate(f.read().splitlines()):
             if i == 0:
                 if 'G' in line:
                     directional = False
@@ -17,12 +17,10 @@ class FileReader(object):
                 tuple_line = self.line_to_tuple(line)
                 edges.append(tuple_line)
                 if directional is False:
-                    edges.append((tuple_line[1], tuple_line[0], tuple_line[2]))
+                    edges.append(
+                        (tuple_line[1], tuple_line[0], *tuple_line[2:]))
         f.close()
         return verticies, edges
-
-    def read_from_d(self, f):
-        pass
 
     def line_to_tuple(self, line):
         stack = []
