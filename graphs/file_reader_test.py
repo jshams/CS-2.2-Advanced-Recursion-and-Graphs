01c2780ac0eb56ae54e177469269a09d9840ff35
Jake Shams <jshams62@gmail.com>
2019-07-19 15:09:54 -0700

    tests for read_file

--- graphs/file_reader_test.py
+++ graphs/file_reader_test.py
@@ -1,12 +1,25 @@
 from file_reader import FileReader
 import unittest
 
+
 class FileReaderTest(unittest.TestCase):
     def test_init(self):
-        f = FileReader('PATH')
-        assert f.PATH == 'PATH'
-    
+        path = 'file_reader_test.txt'
+        f = FileReader(path)
+        assert f.PATH == 'file_reader_test.txt'
+
     def test_line_to_tuple(self):
-        assert FileReader().line_to_tuple('(12,24)') == ( '12','24')
-        assert FileReader().line_to_tuple('(12,24,36)') == ( '12','24','36')
-        assert FileReader().line_to_tuple('(12,24,36,48)') == ( '12','24','36','48')
+        path = 'file_reader_test.txt'
+        assert FileReader(path).line_to_tuple('(12,24)') == ('12', '24')
+        assert FileReader(path).line_to_tuple(
+            '(12,24,36)') == ('12', '24', '36')
+        assert FileReader(path).line_to_tuple(
+            '(12,24,36,48)') == ('12', '24', '36', '48')
+
+    def test_read_file(self):
+        path = 'file_reader_test.txt'
+        f = FileReader(path)
+        verticies, edges = f.read_file()
+        assert verticies == ['1', '2', '3', '4', '5']
+        assert edges == [('1', '2'), ('2', '1'), ('2', '3'), ('3', '2'), ('3', '4'), ('4', '3'),
+                         ('4', '5'), ('5', '4'), ('5', '1'), ('1', '5')]
