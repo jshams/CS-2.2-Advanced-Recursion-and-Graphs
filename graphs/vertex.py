31b625d8098b295f6a844984d3c5fa2576d61c24
Jake Shams <jshams62@gmail.com>
2019-07-19 15:53:33 -0700

    fixed bugs after testing

--- graphs/vertex.py
+++ graphs/vertex.py
@@ -1,14 +1,18 @@
 from linkedlist import LinkedList
 
+
 class Vertex(object):
     def __init__(self, data: str):
         self.data = data
         self.pointers = LinkedList()
 
-    def points_to(self, pointer, weight = 1):
+    def points_to(self, pointer, weight=1):
         '''Adds a pointer to the linked list of pointers'''
-        self.pointers.append((pointer , weight))
+        self.pointers.append((pointer, weight))
 
     def is_pointing_to(self, item: str):
         '''returns a boolean indicating whether the vertex is pointing to another item'''
-        return item in self.pointers
+        for vert in self.pointers:
+            if vert[0].data == item:
+                return True
+        return False
