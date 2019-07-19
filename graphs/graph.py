a38158724af58bd03592af18910c247ad941d30f
Jake Shams <jshams62@gmail.com>
2019-07-19 15:27:07 -0700

    added init methods

--- graphs/graph.py
+++ graphs/graph.py
@@ -3,11 +3,14 @@ from file_reader import FileReader
 
 
 class ADTGraph(object):
-    def __init__(self, FILE_PATH=''):
+    def __init__(self, FILE_PATH):
         self.f = FileReader(FILE_PATH)
         self.vertices = {}  # dictionary of verticies
         self.vertex_count = 0
         self.edge_count = 0
+        self.all_verticies, self.edges = self.f.read_file()
+        self.add_verticies(self.all_verticies)
+        self.add_egdes(self.edges)
 
     def __iter__(self):
         '''yields each vertex key'''
@@ -18,6 +21,10 @@ class ADTGraph(object):
         '''adds an instance of Vertex to the graph.'''
         self.vertices[vert] = Vertex(vert)
         self.vertex_count += 1
+
+    def add_verticies(self, verts):
+        for vert in verts:
+            self.add_vertex(vert)
 
     def add_edge(self, from_vert_key, to_vert_key, weight=1):
         '''Adds a new, weighted, directed edge to the graph that connects two vertices.'''
@@ -30,6 +37,10 @@ class ADTGraph(object):
         else:
             from_vert.points_to(to_vert)
             self.edge_count += 1
+
+    def add_egdes(self, edges):
+        for edge in edges:
+            self.add_edge(*edge)
 
     def get_vertex(self, vert_key):
         '''finds the vertex in the graph named vertKey. Or returns None if not found'''
