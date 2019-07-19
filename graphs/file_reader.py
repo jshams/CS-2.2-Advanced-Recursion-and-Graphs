1e958cf7cd8e41ddba0e5907c6ccd6566aab6d43
Jake Shams <jshams62@gmail.com>
2019-07-19 14:59:00 -0700

    read_file method implemented

--- graphs/file_reader.py
+++ graphs/file_reader.py
@@ -1,8 +1,27 @@
 class FileReader(object):
-    def __init__(self, PATH = None):
+    def __init__(self, PATH=None):
         self.PATH = PATH
-    
-    def read_from_g(self):
+
+    def read_file(self):
+        directional = True
+        verticies = []
+        edges = []
+        f = open(self.PATH, "r")
+        for i, line in enumerate(f.readlines()):
+            if i == 0:
+                if 'G' in line:
+                    directional = False
+            elif i == 1:
+                verticies = line.split(',')
+            else:
+                tuple_line = self.line_to_tuple(line)
+                edges.append(tuple_line)
+                if directional is False:
+                    edges.append((tuple_line[1], tuple_line[0], tuple_line[2]))
+        f.close()
+        return verticies, edges
+
+    def read_from_d(self, f):
         pass
 
     def line_to_tuple(self, line):
