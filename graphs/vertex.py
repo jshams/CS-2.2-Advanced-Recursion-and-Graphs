b5380e7ffa5264aa1e962a3cb2b093aaf4d86e8a
Jake Shams <jshams62@gmail.com>
2019-07-23 20:10:56 -0700

    vertex attr pointers are a dict instead of a linked list

--- graphs/vertex.py
+++ graphs/vertex.py
@@ -4,15 +4,12 @@ from linkedlist import LinkedList
 class Vertex(object):
     def __init__(self, data: str):
         self.data = data
-        self.pointers = LinkedList()
+        self.pointers = dict()
 
     def points_to(self, pointer, weight=1):
         '''Adds a pointer to the linked list of pointers'''
-        self.pointers.append((pointer, weight))
+        self.pointers[pointer] = weight
 
     def is_pointing_to(self, item: str):
         '''returns a boolean indicating whether the vertex is pointing to another item'''
-        for vert in self.pointers:
-            if vert[0] == item:
-                return True
-        return False
+        return item in self.pointers
