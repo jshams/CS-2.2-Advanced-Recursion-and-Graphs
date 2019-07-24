5c8667bbdd802c395ae722c6f404ee25ebc3c673
Jake Shams <jshams62@gmail.com>
2019-07-23 21:34:58 -0700

    implemented breadth first search and shortest path methods

--- graphs/graph.py
+++ graphs/graph.py
@@ -29,29 +29,23 @@ class ADTGraph(object):
         for vert in verts:
             self.add_vertex(vert)
 
+    def get_vertex(self, vert_key):
+        '''finds the vertex in the graph named vertKey. Or returns None if not found'''
+        if vert_key in self.vertices:
+            return self.vertices[vert_key]
+
     def add_edge(self, from_key, to_key, weight=1):
         '''Adds a new, weighted, directed edge to the graph that connects two vertices.'''
-        # from_vert = self.get_vertex(from_vert_key)
-        # to_vert = self.get_vertex(to_vert_key)
-        # if from_vert is None:
-        #     raise KeyError(f'{from_vert} key not found in graph')
-        # elif to_vert is None:
-        #     raise KeyError(f'{to_vert} key not found in graph')
         if from_key not in self.vertices:
             raise KeyError(f'{from_key} vertex not found in graph')
         if to_key not in self.vertices:
             raise KeyError(f'{to_key} vertex not found in graph')
-        self.vertices[from_key].points_to(to_key)
+        self.get_vertex(from_key).points_to(to_key)
         self.edge_count += 1
 
     def add_egdes(self, edges):
         for edge in edges:
             self.add_edge(*edge)
-
-    def get_vertex(self, vert_key):
-        '''finds the vertex in the graph named vertKey. Or returns None if not found'''
-        if vert_key in self.vertices:
-            return self.vertices[vert_key]
 
     def get_vertices(self):
         '''returns the list of all vertices in the graph.'''
@@ -61,15 +55,32 @@ class ADTGraph(object):
         '''lists all vertices y such that there is an edge from the vertex x to the vertex y.'''
         return [pointer for pointer in x.pointers]
 
-    def breadth_first_search(self, start, end):
+    def breadth_first_search(self, start):
         seen = {start}
         queue = Queue([start])
+        print(start)
         while queue.is_empty() is False:
             vertex = queue.dequeue()
-            for pointer in self.vertices[vertex].pointers:
-                print(pointer)
+            for pointer in self.get_vertex(vertex).pointers:
+                if not pointer in seen:
+                    print(pointer)
+                    seen.add(pointer)
+                    queue.enqueue(pointer)
+
+    def shortest_path(self, a, b):
+        seen = {a: []}
+        queue = Queue(a)
+        while queue.is_empty() is False:
+            vertex = queue.dequeue()
+            for pointer in self.get_vertex(vertex).pointers:
+                if not pointer in seen:
+                    if pointer == b:
+                        return seen[vertex] + [vertex, pointer]
+                    seen[pointer] = seen[vertex] + [vertex]
+                    queue.enqueue(pointer)
 
 
 if __name__ == '__main__':
     g = ADTGraph('file_reader_test.txt')
-    g.breadth_first_search('1', '2')
+    shotest_path = g.shortest_path('5', '2')
+    print(shotest_path)
