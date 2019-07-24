884d0b49bc4c666a53382a47c8e6b3621bf33903
Jake Shams <jshams62@gmail.com>
2019-07-23 22:17:26 -0700

    setup for challenge 3

---
+++ graphs/challenge3.py
@@ -0,0 +1,14 @@
+import sys
+from graph import ADTGraph
+
+if __name__ == '__main__':
+    PATH = sys.argv[1][:-4] + '_c3' + sys.argv[1][-4:]
+    a = sys.argv[2]
+    b = sys.argv[3]
+    g = ADTGraph(PATH)
+    has_path = g.recursive_dfs(a, b)
+    shortest_weighted_path = g.dijkstras(a, b)
+    print(
+        f'There exists a path between vertex {a} and {b}: {"TRUE" if has_path else "FALSE"}')
+    print(
+        f'The weight of the minimum weight path between vertex {a} and {b} is: {shortest_weighted_path}')
