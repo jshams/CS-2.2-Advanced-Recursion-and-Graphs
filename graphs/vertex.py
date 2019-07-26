bd9f7967fc4ef7cf3e018d1a6ad0c7a589d14c83
Jake Shams <jshams62@gmail.com>
2019-07-26 10:15:59 -0700

    pointers is now neighbors

--- graphs/vertex.py
+++ graphs/vertex.py
@@ -4,12 +4,19 @@
 class Vertex(object):
     def __init__(self, data: str):
         self.data = data
-        self.pointers = dict()
+        self.neighbors = dict()
 
-    def points_to(self, pointer, weight=1):
-        '''Adds a pointer to the linked list of pointers'''
-        self.pointers[pointer] = weight
+    def points_to(self, neighbor_key, weight=1):
+        '''Adds a edge from this vertex to the given neighboring vertex with the given weight'''
+        self.neighbors[neighbor_key] = weight
 
-    def is_pointing_to(self, item: str):
-        '''returns a boolean indicating whether the vertex is pointing to another item'''
-        return item in self.pointers
+    def is_pointing_to(self, key: str):
+        '''Returns a boolean indicating whether the vertex is pointing to another item'''
+        return key in self.neighbors
+
+    def __hash__(self):
+        return hash(self.data)
+
+
+# {'A': {'B': 53, 'C': 12}, 'C': {'A': 12}, 'B': {}}
+# {'A': Vertex(data='A', neighbors={'B': 53, 'C': 12}), ...}
