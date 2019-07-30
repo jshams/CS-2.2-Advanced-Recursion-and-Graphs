e46d0cf965667efc485d8a13d7276ae86c9b466a
Jake Shams <jshams62@gmail.com>
2019-07-29 17:49:29 -0700

    added tests for is_digraph and updates readfile

--- graphs/file_reader_test.py
+++ graphs/file_reader_test.py
@@ -7,19 +7,26 @@ class FileReaderTest(unittest.TestCase):
         path = 'file_reader_test.txt'
         f = FileReader(path)
         assert f.PATH == 'file_reader_test.txt'
+        assert len(f.edges) > 0
+        assert len(f.verticies) > 0
+
+    def test_is_digraph(self):
+        path = 'file_reader_test.txt'
+        f = FileReader(path)
+        assert f.PATH == 'file_reader_test.txt'
+        assert f.digraph == False
 
     def test_line_to_tuple(self):
-        path = 'file_reader_test.txt'
-        assert FileReader(path).line_to_tuple('(12,24)') == ('12', '24')
-        assert FileReader(path).line_to_tuple(
+        f = FileReader()
+        assert f.line_to_tuple('(12,24)') == ('12', '24')
+        assert f.line_to_tuple(
             '(12,24,36)') == ('12', '24', '36')
-        assert FileReader(path).line_to_tuple(
+        assert f.line_to_tuple(
             '(12,24,36,48)') == ('12', '24', '36', '48')
 
     def test_read_file(self):
         path = 'file_reader_test.txt'
         f = FileReader(path)
-        verticies, edges = f.read_file()
-        assert verticies == ['1', '2', '3', '4', '5']
-        assert edges == [('1', '2'), ('2', '1'), ('2', '3'), ('3', '2'), ('3', '4'), ('4', '3'),
-                         ('4', '5'), ('5', '4'), ('5', '1'), ('1', '5')]
+        assert f.verticies == ['1', '2', '3', '4', '5']
+        assert f.edges == [('1', '2'), ('2', '1'), ('2', '3'), ('3', '2'), ('3', '4'), ('4', '3'),
+                           ('4', '5'), ('5', '4'), ('5', '1'), ('1', '5')]
