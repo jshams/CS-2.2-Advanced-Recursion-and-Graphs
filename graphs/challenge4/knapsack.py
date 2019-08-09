d9e54e89c29946133e79f7247020f5bb05886f78
Jake Shams <jshams62@gmail.com>
2019-08-09 02:52:43 -0700

    docstrings and more

--- graphs/challenge4/knapsack.py
+++ graphs/challenge4/knapsack.py
@@ -70,6 +70,12 @@
 #         return best_value, list(best_combo)
 
 def recursive_knapsack(items, capacity):
+    '''inputs:
+            an array of tuples with weights and values
+            maximum capacity of the bag
+        output:
+            the max value that can fit in the bag
+        '''
     if len(items) == 0 or capacity == 0:
         return 0
     item = items[0]
@@ -82,19 +88,24 @@ def recursive_knapsack(items, capacity):
 
 
 def fast_knapsack(items, capacity, items_so_far=None):
+    '''inputs:
+        an array of tuples with weights and values
+        maximum capacity of the bag
+        items_so_far default to None
+    output:
+        the max value that can fit in the bag, the items in that combination'''
     if items_so_far is None:
         items_so_far = items.copy()
     if len(items) == 0 or capacity == 0:
         return 0, items_so_far
     item = items[0]
-    items_so_far_without = items_so_far.copy()
-    items_so_far_without.remove(item)
+    items_so_far_without = items_so_far[1:]
     if item[0] > capacity:
-        return fast_knapsack(items[1:], capacity, items_so_far_without)
+        return fast_knapsack(items_so_far_without, capacity, items_so_far_without)
     value_without = fast_knapsack(
-        items[1:], capacity, items_so_far_without)
+        items_so_far_without, capacity, items_so_far_without)
     value_with = fast_knapsack(
-        items[1:], capacity - item[0], items_so_far.copy())
+        items_so_far_without, capacity - item[0], items_so_far.copy())
     value_with = (value_with[0] + item[1], value_with[1])
     if value_with[0] > value_without[0]:
         return value_with
@@ -103,5 +114,15 @@ def fast_knapsack(items, capacity, items_so_far=None):
 
 
 if __name__ == '__main__':
-    good_call = fast_knapsack([(10, 2), (4, 2), (2, 6), (3, 2), (10, 500)], 15)
-    print(good_call)
+    g = fast_knapsack([(10, 2), (4, 2), (2, 6), (3, 2), (10, 500)], 15)
+    print(g)
+
+    # sack_capacity = 6404180
+    # object_weights = [382745, 799601, 909247, 729069, 467902, 44328, 34610,
+    #                   698150, 823460, 903959, 853665, 551830, 610856, 670702,
+    #                   488960, 951111, 323046, 446298, 931161, 31385, 496951,
+    #                   264724, 224916, 169684]
+    # object_profits = [825594, 1677009, 1676628, 1523970, 943972, 97426, 69666,
+    #                   1296457, 1679693, 1902996, 1844992, 1049289, 1252836,
+    #                   1319836, 953277, 2067538, 675367, 853655, 1826027, 65731,
+    #                   901489, 577243, 466257, 369261]
