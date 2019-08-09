9546d56edce30e177887682049c3e5e902d1032b
Jake Shams <jshams62@gmail.com>
2019-08-09 03:34:04 -0700

    slightly faster

--- graphs/challenge4/towers_of_hanoi.py
+++ graphs/challenge4/towers_of_hanoi.py
@@ -4,14 +4,11 @@ seen = {}
 def towers_of_hanoi_moves(n):
     if n == 1:
         return [(1, 3)]
-    elif n == 2:
-        return [(1, 2), (1, 3), (2, 3)]
     else:
         if n not in seen:
             toh_n_minus_one = towers_of_hanoi_moves(n - 1)
-            seen[n] = pivot(toh_n_minus_one, 2, 3) + \
-                towers_of_hanoi_moves(
-                1) + pivot(toh_n_minus_one, 1, 2)
+            pivoted = pivot_twice(toh_n_minus_one)
+            seen[n] = pivoted[0] + [(1, 3)] + pivoted[1]
         return seen[n]
 
 
@@ -30,9 +27,11 @@ def pivot(lst_of_tuples, switch1, switch2):
     return [swap(tup, switch1, switch2) for tup in lst_of_tuples]
 
 
+def pivot_twice(lst_of_tuples):
+    return [swap(tup, 2, 3) for tup in lst_of_tuples], [swap(tup, 1, 2) for tup in lst_of_tuples]
+
+
 def swap(tup, switch1, switch2):
-    index0 = None
-    index1 = None
     if tup[0] == switch1:
         index0 = switch2
     elif tup[0] == switch2:
@@ -49,5 +48,13 @@ def swap(tup, switch1, switch2):
 
 
 if __name__ == '__main__':
-    z = towers_of_hanoi_moves(26)
-    print(len(z))
+    import sys
+    n = 3
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+    z = towers_of_hanoi_moves(n)
+    print(
+        f'With {n} rings in our tower the minumum number of moves is: {len(z)}')
+    print('And the moves are:')
+    # [print(f'{i + 1}. {y}') for i, y in enumerate(z)]
+    print(z)
